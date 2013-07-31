01fdb62e2b6ce30353ada319e87e97e8130e64c2
allenzhang <hb.zhanglu@gmail.com>
2013-08-01 00:31:51 +0800

    0801

--- yjssm/view/index.php
+++ yjssm/view/index.php
@@ -1,13 +1,15 @@
 <?php require_once dirname(__FILE__) . '/inc/header.html'; ?>
 
 		<div class="row-fluid">
-			<div class="span10 offset1" style="background-color:#eee;border-radius:5px;margin-top:10px;margin-bottom:10px;">
-				<div class="span3" style="width:25%;height:250px;margin: 25px 10px 10px 40px;line-height: 30px;">	
-					<h4 style="text-align:center">衡水永佳电动伸缩门配件厂</h4>公司已有十余年的发展历史，主要生产电动伸缩门配件（轮类，轮罩，机头轮，行走轮等），专业维修电动伸缩门，在市场经济的大潮下，本厂跻身国内大型专业生产厂家的行列，生产的铸铁轮，橡胶轮等产品被许多国家大中型重点工程所采用，取得了经济效益和社会效益的双丰收。
-					<p style="text-align:right;color:orange"><a href="#">更多信息&gt;&gt;</a></p>
+			<div class="span1"></div>
+			<div class="span10" style="margin-top:20px">
+				<div style="background-color:#eee;width:33.3%;height: 300px;float:left;">
+					<div style="margin:25px;line-height:30px;">	
+						<h4 style="text-align:center">衡水永佳电动伸缩门配件厂</h4>公司已有十余年的发展历史，主要生产电动伸缩门配件（轮类，轮罩，机头轮，行走轮等），专业维修电动伸缩门，在市场经济的大潮下，本厂跻身国内大型专业生产厂家的行列，生产的铸铁轮，橡胶轮等产品被许多国家大中型重点工程所采用，取得了经济效益和社会效益的双丰收。
+						<p style="text-align:right;"><a href="#">更多信息&gt;&gt;</a></p>
+					</div>
 				</div>
-
-				<div name="Carousel_img" style="width:67%;float:left;display:block;margin:20px 10px 5px 10px">
+				<div name="Carousel_img" style="width:66.7%;float:right;">
 					<div id="myCarousel" class="carousel slide" >
 						<ol class="carousel-indicators">
 							<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -27,31 +29,24 @@
 			</div>
 		</div>	
 
-
-
-
 		<div class="row-fluid">
-			<ul class="thumbnails">
-			<?php $index = 0;foreach($this->arrImg[4] as $img){$index++;?>
-			<li class="span2 <?php if(1==$index) echo 'offset1'; ?>">
-				<a class="thumbnail" href="#" style="background-color: #eee;">
-				<div style="text-align: center;padding-top:12px">
-					<img src="<?php echo $img['path'];?>">
-					<h5 style="text-align:center;color:black"><?php echo $img['description']; ?></h5>
+			<div class="span1"></div>
+			<?php foreach($this->arrImg[4] as $img){?>
+			<div class="span2">
+				<div style="background-color:#eee;padding-top:5px;padding-bottom:2px;">
+					<div style="margin:0 auto;margin-top:10px;border:1px solid #ddd;width:130px;height:130px;text-align:center;padding:3px">
+						<img width="130px" height="130px" src="<?php echo $img['path'];?>">
+					</div>
+					<div style="text-align:center;margin-top:10px;margin-bottom:10px"><?php echo $img['description']; ?></div>
 				</div>
-				</a>
-			</li>
+			</div>
 			<?php }?>
-
-			</ul>
+			</div>
 		</div>
-
-		<div style="width:83%;height:1px;margin:0px auto;padding:0px;background-color:#D5D5D5;overflow:hidden;"></div>
-
-
+		<div style="width:83%;height:1px;margin:0px auto;margin-top:20px;margin-bottom:19px;background-color:#D5D5D5;"></div>
 
 		<div class="row-fluid">
-			<div class="span10 offset1" style="margin-top:25px">
+			<div class="span10 offset1">
 				<?php $index = 0;foreach($this->arrImg[5] as $img){$index++;?>
 				<div class="span4 shadow-box">
 				<a href="#" class="thumbnail"><img style="width:100%;height:100%" src="<?php echo $img['path']?>"></a>
--- yjssm/view/news_view.php
+++ yjssm/view/news_view.php
@@ -1,14 +1,16 @@
 <?php require_once dirname(__FILE__) . '/inc/header.html'; ?>
 
-<div class="row-fluid">
-	<div class="span7 offset1 page">
-		<div><h4><?php echo $this->news['title']; ?></h4></div>
-		<div><?php echo $this->news['pub_time']; ?>&nbsp;&nbsp;来源：<?php echo $this->news['source'];?></div>
-
-		<div>
-			<?php if($this->news && $this->news['imgInfo']){?>
-				<img style="width:520px;height:280px" src="<?php echo $this->news['imgInfo'][0]['path'] ?>">
-			<?php }?>	
+<div class="row-fluid" style="margin-top:20px">
+	<div class="span1"></div>
+	<div class="span7">
+		<div style="text-align:center">
+			<div><h4><?php echo $this->news['title']; ?></h4></div>
+			<div><?php echo $this->news['pub_time']; ?>&nbsp;&nbsp;来源：<?php echo $this->news['source'];?></div>
+			<div>
+				<?php if($this->news && $this->news['imgInfo']){?>
+					<img style="width:520px;height:280px" src="<?php echo $this->news['imgInfo'][0]['path'] ?>">
+				<?php }?>	
+			</div>
 		</div>
 		<div class="row-fluid">
 			<div class="span10 offset1" style="font-size:14px"><?php echo $this->news['content']; ?></div>
